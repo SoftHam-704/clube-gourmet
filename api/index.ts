3ea6c0d6cd9b34ff1381c8891aff5b770cbbe464
Softham <hamilton@softham.com.br>
2026-04-12 10:06:03 -0400

    fix: ajuste no valor do runtime para o Vercel

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { handle } from '@hono/node-server/vercel';
 import app from '../src/api/index.js';
 
 export const config = {
-  runtime: 'nodejs18.x',
+  runtime: 'nodejs',
 };
 
 export default handle(app);
